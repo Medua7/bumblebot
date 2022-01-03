9d669423a89ae2e4ec032ee69b1f527e1bc74633
Bumble <bumbletrixx@outlook.com>
2022-01-03 23:26:12 +0100

    Updated foreverjs

--- forever.js
+++ forever.js
@@ -1,11 +1,18 @@
-var forever = require('forever-monitor');
+var forever = require('forever');
 
 const FILE = 'index.js';
 
 var child = new (forever.Monitor)(FILE, { silent: true });
 
 child.on('exit', function () {
-    console.log(FILE+' has exited after 3 restarts');
+    console.log(FILE+' has exited!');
+});
+child.on('restart', function () {
+    console.log(FILE+' has restarted!');
+});
+child.on('watch:restart', function () {
+    console.log(FILE+' has restarted, because the script changed!');
 });
 
 child.start();
+forever.startServer(child);
